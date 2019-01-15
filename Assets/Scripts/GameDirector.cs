4ddfcfa72a50f53329a981d89d87e201f039c864
Michaël St-Pierre <michael@allierh.com>
2019-01-14 21:08:30 -0500

    Add random peon spawn on left click

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            Instantiate(
+                Resources.Load("Prefabs/PeonLeft"), 
+                new Vector3(Random.Range(-8f, -5f), 1.69f, Random.Range(-4f, 12f)), 
+                new Quaternion(0, 0, 0, 0)
+            );
+        }
+    }
+}
