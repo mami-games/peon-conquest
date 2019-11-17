cf91ba62e85154bb5a2d43decc6eabd45baffff6
Michaël St-Pierre <chimp.micstp@gmail.com>
2019-11-17 16:09:14 -0500

    Experimentations

---
+++ Assets/Scripts/Peon/PeonAI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+public class PeonAI : MonoBehaviour
+{
+    public GameObject currentTarget;
+    public float mouvementSpeed = 3f;
+    public int health = 100;
+    public int armor = 35;
+    private float actionZoneRadius = 1.5f;
+
+    private void Start() {
+        GetComponentInChildren<WeaponAnimation>().target = currentTarget.GetComponent<PeonAI>();
+    }
+
+    void Update() {
+        if(currentTarget != null) {
+            LookAtCurrentTarget();
+            MoveTowardsCurrentTarget();
+        }
+
+        GetComponentInChildren<Animator>().SetBool("isAttacking", currentTarget != null && CurrentTargetIsReached());
+    }
+
+    private void LookAtCurrentTarget() {
+        transform.LookAt(currentTarget.transform);
+    }
+
+    public void TakeDamage(int damage) {
+        health -= (armor < damage) ? (damage - armor) : 1;
+
+        if(health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void MoveTowardsCurrentTarget() {
+        if(!CurrentTargetIsReached()) {
+            transform.position = Vector3.MoveTowards(
+                transform.position, 
+                currentTarget.transform.position, 
+                GetDeltaMouvementSpeed()
+            );
+        }
+    }
+
+    private bool CurrentTargetIsReached() {
+        return GetDistanceFromCurrentTarget() <= actionZoneRadius;
+    }
+
+    private float GetDeltaMouvementSpeed() {
+        return mouvementSpeed * Time.deltaTime;
+    }
+
+    private float GetDistanceFromCurrentTarget() {
+        return Vector3.Distance(
+            transform.position, 
+            currentTarget.transform.position
+        );
+    }
+}
---
+++ Assets/Scripts/WeaponAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class WeaponAnimation : MonoBehaviour
+{
+    public PeonAI target;
+    public int damage = 35;
+
+    public void OnAttackAnimationHit() {
+        target.TakeDamage(damage);
+    }
+}
